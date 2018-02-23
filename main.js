3043c5a90978ee3718e81a16e2b80ee84879c53a
Wioletta Gluza <wiolagluza@gmail.com>
2018-02-23 18:28:12 +0100

    JSON talk to me

--- main.js
+++ main.js
@@ -1,49 +1,59 @@
-var canvas = document.querySelector('canvas');
-
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-
-var c = canvas.getContext('2d');
-
-// multiple circle "stars"
-for (var i = 0; i< 400; i++) {
-  var x = Math.random() * window.innerWidth;
-  var y = Math.random() * window.innerHeight;
-  var r = Math.floor(Math.random() * 0.01 ) + 1.5;
-  c.beginPath();
-  c.arc(x, y, r, 0, Math.PI * 2, false);
-  c.fillStyle = 'red';
-  c.fill();
+function getData(){
+  fetch("http://www.astro-phys.com/api/de406/records");
+  .then(res => res.json())
+  .then(showBodies);
 }
-
-// Planets coordinates
-var x = 200;
-var y = 200
-
-// dx = velocity
-var dx = 5;
-var dy = 5;
-
-var radius = 40;
-
-function animate() {
-  requestAnimationFrame(animate);
-   c.clearRect(0,0, innerWidth, innerHeight);
-
-  c.beginPath();
-  c.arc(x, y, radius, 0, Math.PI * 2, false);
-  c.fillStyle = 'green';
-  c.fill();
-
-  if  (x > innerWidth || x < 0){
-    dx = -dx;
-  }
-  if  (y > innerHeight || y < 0){
-    dy = -dy;
-  }
-  x += dx;
-  y += dy;
-
+function showBodies(data){
+console.log(data)
 }
-
-animate();
+getData();
+//
+// var canvas = document.querySelector('canvas');
+//
+// canvas.width = innerWidth;
+// canvas.height = innerHeight;
+//
+// var c = canvas.getContext('2d');
+//
+// // multiple circle "stars"
+// for (var i = 0; i< 400; i++) {
+//   var x = Math.random() * window.innerWidth;
+//   var y = Math.random() * window.innerHeight;
+//   var r = Math.floor(Math.random() * 0.01 ) + 1.5;
+//   c.beginPath();
+//   c.arc(x, y, r, 0, Math.PI * 2, false);
+//   c.fillStyle = 'red';
+//   c.fill();
+// }
+//
+// // Planets coordinates
+// var x = 200;
+// var y = 200
+//
+// // dx = velocity
+// var dx = 5;
+// var dy = 5;
+//
+// var radius = 40;
+//
+// function animate() {
+//   requestAnimationFrame(animate);
+//    c.clearRect(0,0, innerWidth, innerHeight);
+//
+//   c.beginPath();
+//   c.arc(x, y, radius, 0, Math.PI * 2, false);
+//   c.fillStyle = 'green';
+//   c.fill();
+//
+//   if  (x > innerWidth || x < 0){
+//     dx = -dx;
+//   }
+//   if  (y > innerHeight || y < 0){
+//     dy = -dy;
+//   }
+//   x += dx;
+//   y += dy;
+//
+// }
+//
+// animate();
